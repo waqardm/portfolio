37215f40b05393f5d7c6b778411f34d25aed0104
Waqar Mohammad <waqar@waqar.co>
2018-09-03 15:18:25 +0100

    Working contact form with validation and sending feature

---
+++ dist/handler.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler();
+$mailer = $pp->getMailer();
+$mailer->setFrom('lawyerscode@waqar.co','Form',false);
+
+
+$validator = $pp->getValidator();
+$validator->fields(['name', 'email','message'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+
+
+$pp->sendEmailTo('lawyerscode@waqar.co'); // ← Your email here
+
+echo $pp->process($_POST);
---
+++ dist/js/contact.js
@@ -0,0 +1,53 @@
+
+$(function () {
+    function after_form_submitted(data) {
+        if (data.result == 'success') {
+            $('form#form').hide();
+            $('#success-message').show();
+            $('#error-message').hide();
+        } else {
+            $('#error-message').append('<ul></ul>');
+
+            jQuery.each(data.errors, function (key, val) {
+                $('#error-message ul').append('<li>' + key + ':' + val + '</li>');
+            });
+            $('#success-message').hide();
+            $('#error-message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function () {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if (label) {
+                    $btn.prop('type', 'submit');
+                    $btn.text(label);
+                    $btn.prop('orig_label', '');
+                }
+            });
+
+        } //else
+    }
+
+    $('#form').submit(function (e) {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function () {
+            $btn = $(this);
+            $btn.prop('type', 'button');
+            $btn.prop('orig_label', $btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+        $.ajax({
+            type: "POST",
+            url: 'handler.php',
+            data: $form.serialize(),
+            success: after_form_submitted,
+            dataType: 'json'
+        });
+
+    });
+});
